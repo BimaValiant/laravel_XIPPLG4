132e70350f5198af86c54ccbeceda87331aa68e7
BimaValiant <bimavaliant@gmail.com>
2025-10-21 15:15:35 +0700

    implement landing page

--- myweb/resources/js/app.js
+++ myweb/resources/js/app.js
@@ -1,1 +1,4 @@
-import './bootstrap';
+import 'admin-lte';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import 'admin-lte/dist/css/adminlte.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
---
+++ myweb/resources/views/partials/navbar.blade.php
@@ -0,0 +1,5 @@
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <ul class="navbar-nav">
+        <li class="nav-item">Navbar</li>
+    </ul>
+</nav>
